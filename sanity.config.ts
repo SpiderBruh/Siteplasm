e5de9e379f2e0f39df1267496322ca92e854d1f1
kristaezekiel28 <le.advena08@gmail.com>
2026-05-02 10:43:23 +0800

    Switch to rock-solid Sanity v3 stack

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,7 @@
 
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
-import { structureTool } from 'sanity/structure';
+import { deskTool } from 'sanity/desk';
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from './src/sanity/env';
@@ -19,7 +19,7 @@ export default defineConfig({
   // Add and edit the content schema in the './src/sanity/schema' directory
   schema,
   plugins: [
-    structureTool(),
+    deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
